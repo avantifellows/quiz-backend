0ab06123ea11d30c86fa5be0ec6d2043ad7d45f5
SaloniChoudhary <salonichoudhary1999@gmail.com>
2022-04-13 13:39:25 +0530

    added pre-commit.ci

--- app/database.py
+++ app/database.py
@@ -1,3 +1,5 @@
 from pymongo import MongoClient
+
 client = MongoClient(
-    "mongodb+srv://quiz:p$#p4h7y_Z44R-n@cluster0.uocfg.mongodb.net/quiz?retryWrites=true&w=majority")
+    "mongodb+srv://quiz:p$#p4h7y_Z44R-n@cluster0.uocfg.mongodb.net/quiz?retryWrites=true&w=majority"
+)
