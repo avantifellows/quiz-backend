4c2d74a67be27a198f87e2d46ccee1b865232ad9
SaloniChoudhary <salonichoudhary1999@gmail.com>
2022-04-14 17:06:05 +0530

    added .env

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
 from pymongo import MongoClient
-client = MongoClient(
-    "mongodb+srv://quiz:p$#p4h7y_Z44R-n@cluster0.uocfg.mongodb.net/quiz?retryWrites=true&w=majority")
+import os
+
+client = MongoClient(os.environ.get("MONGO_CLIENT"))
