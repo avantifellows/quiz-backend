c0f83f79774920b6f46ee5beb0af671f7654a122
Aman Dalmia <amandalmia18@gmail.com>
2022-04-27 18:00:38 +0530

    DEBUG: added print statements to database.py

--- app/database.py
+++ app/database.py
@@ -4,9 +4,11 @@ from pymongo import MongoClient
 # this is required for loading environment variables when
 # running the app locally as the environment variable should
 # be set when the app is running on staging/production by Github Actions
+print(os.environ)
 if "MONGO_AUTH_CREDENTIALS" not in os.environ:
     from dotenv import load_dotenv
 
+    print("entered here")
     load_dotenv("../.env")
 
 client = MongoClient(os.getenv("MONGO_AUTH_CREDENTIALS"))
