0a97cdd1d20007423f7866f8faeeaaec34abd634
SaloniChoudhary <salonichoudhary1999@gmail.com>
2022-05-04 15:46:40 +0530

    test added for quizzes.py

--- app/tests/test_quizzes.py
+++ app/tests/test_quizzes.py
@@ -16,21 +16,23 @@ class QuestionsTestCase(unittest.TestCase):
     def tearDownClass(cls):
         disconnect()
 
-    def test_to_create_quiz(self):
+    def setUp(self):
         data = open("app/dummy_data/homework_quiz.json")
-        quiz_data = json.load(data)
-        response = client.post("/quiz/", json=quiz_data)
-        quiz_data = json.loads(response.content)
+        self.quiz_data = json.load(data)
+        response = client.post("/quiz/", json=self.quiz_data)
+        response = json.loads(response.content)
+        self.id = response["_id"]
+
+    def test_to_create_quiz(self):
+        response = client.post("/quiz/", json=self.quiz_data)
         assert response.status_code == 201
+
+    def test_get_question_if_id_valid(self):
+        response = client.get(f"/quiz/{self.id}")
+        assert response.status_code == 200
 
     def test_get_quiz_returns_error_if_id_invalid(self):
         response = client.get("/quiz/00")
         assert response.status_code == 404
         response = response.json()
         assert response["detail"] == "quiz 00 not found"
-
-    def test_get_question_if_id_valid(self):
-        id = self.quiz_data["_id"]
-        response = client.get(f"/quiz/{id}")
-        question = response.json()
-        assert question == self.quiz_data
