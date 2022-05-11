e38f6ea1f4267e82237fd8008c2a9d4f1d10bce3
SaloniChoudhary <salonichoudhary1999@gmail.com>
2022-05-11 10:09:36 +0530

    quiz tests updated

--- app/tests/test_quizzes.py
+++ app/tests/test_quizzes.py
@@ -7,7 +7,7 @@ from main import app
 client = TestClient(app)
 
 
-class QuestionsTestCase(unittest.TestCase):
+class QuizTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         connect("mongoenginetest", host="mongomock://127.0.0.1:8000")
@@ -22,14 +22,21 @@ class QuestionsTestCase(unittest.TestCase):
         response = client.post("/quiz/", json=self.quiz_data)
         response = json.loads(response.content)
         self.id = response["_id"]
+        self.length = len(response["question_sets"][0]["questions"])
 
-    def test_to_create_quiz(self):
+    def test_create_quiz(self):
         response = client.post("/quiz/", json=self.quiz_data)
-        assert response.status_code == 201
+        response = json.loads(response.content)
+        id = response["_id"]
+        response = client.get(f"/quiz/{id}")
+        assert response.status_code == 200
 
     def test_get_question_if_id_valid(self):
         response = client.get(f"/quiz/{self.id}")
         assert response.status_code == 200
+        response = response.json()
+        question_len = len(response["question_sets"][0]["questions"])
+        assert question_len == self.length
 
     def test_get_quiz_returns_error_if_id_invalid(self):
         response = client.get("/quiz/00")
