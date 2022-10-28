1fcf30425b95bea1bed87d628d296f3078315391
Deepansh Mathur <deepansh.mathur96@gmail.com>
2022-10-28 19:07:27 +0530

    small addition to factor in the subset pattern

--- app/scripts/find_and_replace_some_text_in_all_quizzes.py
+++ app/scripts/find_and_replace_some_text_in_all_quizzes.py
@@ -14,6 +14,9 @@ if __name__ == "__main__":
     changed_questions = []
     changed_quizzes = set()
 
+    # search in the quiz collection, and in each quiz and each question set, find a the question
+    # which are the respective text you're looking to replace
+    # then update the question inside the quiz and separately in the questions collection as well
     for quiz_index, quiz in enumerate(quiz_collection.find()):
         quiz_id = quiz["_id"]
         does_quiz_need_to_change = False
@@ -34,6 +37,17 @@ if __name__ == "__main__":
         if does_quiz_need_to_change:
             print("quiz needs to be updated")
             quiz_collection.update_one({"_id": quiz_id}, {"$set": quiz})
+
+    # search in the questions collection
+    # we need to because the new subset pattern will cause not all questions ka text to exist in the QUIZZES collection
+    # so we need to check the QUESTIONS collection as well
+    for question_index, question in enumerate(question_collection.find()):
+        if "text" in question and "\u20d7" in question["text"]:
+            question["text"] = question["text"].replace("\u20d7", "\\vec.")
+            # update the question also inside question collection
+            question_collection.update_one(
+                {"_id": question["_id"]}, {"$set": {"text": question["text"]}}
+            )
 
     print("DONE!")
     print(len(changed_questions))
