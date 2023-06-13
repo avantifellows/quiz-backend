787c846ccda131611e9797ca68f8d32427e313aa
Deepansh Mathur <deepansh.mathur96@gmail.com>
2023-06-13 11:28:04 +0530

    Added test for colliding api key

--- app/tests/test_organization.py
+++ app/tests/test_organization.py
@@ -1,5 +1,6 @@
 from .base import BaseTestCase
 from fastapi import status
+from unittest.mock import patch
 
 
 class OrganizationsTestCase(BaseTestCase):
@@ -14,6 +15,27 @@ class OrganizationsTestCase(BaseTestCase):
         assert response.status_code == status.HTTP_201_CREATED
         new_organization = response.json()
         assert new_organization["name"] == new_organization_data["name"]
+
+    @patch("routers.organizations.generate_random_string", return_value="mocked_key")
+    def test_create_organization_with_duplicate_api_key(self, mock):
+        # Create the first organization with mocked_key
+        new_organization_data = {
+            "name": "Test Org 2",
+        }
+        response = self.client.post("/organizations/", json=new_organization_data)
+        assert response.status_code == status.HTTP_201_CREATED
+        new_organization = response.json()
+        assert new_organization["name"] == new_organization_data["name"]
+        assert new_organization["key"] == "mocked_key"
+
+        # Try creating a second organization with the same key
+        new_organization_data = {
+            "name": "Test Org 3",
+        }
+        response = self.client.post("/organizations/", json=new_organization_data)
+        assert response.status_code == status.HTTP_409_CONFLICT
+        response = response.json()
+        assert response["detail"] == "API key mocked_key already exists"
 
     def test_authenticate_valid_api_key(self):
         response = self.client.get(
