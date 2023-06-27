58ca87ca82313bc05bae24529cf55aed7c45d44d
Deepansh Mathur <deepansh.mathur96@gmail.com>
2023-06-27 19:51:52 +0530

    turned off logger propogation

--- app/logger_config.py
+++ app/logger_config.py
@@ -15,6 +15,9 @@ class ISTFormatter(logging.Formatter):
 def setup_logger():
     logger = logging.getLogger("quizenginelogger")
 
+    # https://stackoverflow.com/questions/50909824/getting-logs-twice-in-aws-lambda-function
+    logger.propagate = False
+
     logger_format = "%(asctime)s IST loglevel=%(levelname)-6s filename=%(filename)s funcName=%(funcName)s() L%(lineno)-4d %(message)s call_trace=%(pathname)s L%(lineno)-4d"
 
     formatter = ISTFormatter(fmt=logger_format, datefmt="%Y-%m-%d %H:%M:%S")
