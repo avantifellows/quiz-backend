48d25a6d0cb3840400d2e3bcc7b5efae4895e64e
Deepansh Mathur <deepansh.mathur96@gmail.com>
2026-07-16 14:03:17 +0530

    address ECS deployment review feedback

--- terraform/prod/alb.tf
+++ terraform/prod/alb.tf
@@ -21,7 +21,7 @@ resource "aws_lb_target_group" "quiz_backend" {
   vpc_id      = data.aws_vpc.default.id
   target_type = "ip" # Required for Fargate
 
-  deregistration_delay = 5
+  deregistration_delay = 30
 
   health_check {
     enabled             = true
